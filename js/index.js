0ec50be62bcddea43355689e7cd3f096f95a6a65
HCHEN <hchen@garlandtx.gov>
2018-05-04 13:45:30 -0500

    Crime data displayed

--- js/index.js
+++ js/index.js
@@ -65,12 +65,12 @@ require([
       where: "NUM>0",
       displayID: "3"
     }],
-    serviceZoneSourceList: [ {
+    serviceZoneSourceList: [{
         name: "Police Sectors",
         containerID: 1,
         displayID: 2,
         url: "https://maps.garlandtx.gov/arcgis/rest/services/CityMap/Public_Safety/MapServer/5"
-      }, 
+      },
       // {
       //   name: "Police Districts",
       //   containerID: 1,
@@ -224,6 +224,10 @@ require([
       multiSearch.searchResult.addressID = e.result.feature.attributes.Ref_ID;
       multiSearch.searchResult.address = e.result.name;
       multiSearch.getInforByAddressID();
+
+
+      getCrimeData(e.result.feature.geometry);
+      showSubMap(e.result.feature.geometry);
 
       // projecting using geometry service:
       //"project search result, make it under stateplane. ");
@@ -385,7 +389,6 @@ require([
   });
 
   //open crime page
-  //getCrimeData(e.result.feature.geometry);
   function getCrimeData(val) {
     var lat = val.latitude;
     var long = val.longitude;
@@ -408,13 +411,17 @@ require([
     // var url = "https://www.crimereports.com/api/crimes/details.json?agency_id=41082&days=sunday,monday,tuesday,wednesday,thursday,friday,saturday&end_time=23&incident_types=Assault,Assault+with+Deadly+Weapon,Breaking+%26+Entering,Disorder,Drugs,Homicide,Kidnapping,Liquor,Other+Sexual+Offense,Property+Crime,Property+Crime+Commercial,Property+Crime+Residential,Quality+of+Life,Robbery,Sexual+Assault,Sexual+Offense,Theft,Theft+from+Vehicle,Theft+of+Vehicle&include_sex_offenders=false&sandbox=false&start_time=0&zoom=15&start_date=".concat(start_date, "&end_date=", end_date, "&lat1=", lat1, "&lat2=", lat2, "&lng1=", long1, "&lng2=", long2);
 
     var url = "https://www.crimereports.com/home/#!/dashboard?zoom=15&searchText=Garland%252C%2520Texas%252075040%252C%2520United%2520States&incident_types=Assault%252CAssault%2520with%2520Deadly%2520Weapon%252CBreaking%2520%2526%2520Entering%252CDisorder%252CDrugs%252CHomicide%252CKidnapping%252CLiquor%252COther%2520Sexual%2520Offense%252CProperty%2520Crime%252CProperty%2520Crime%2520Commercial%252CProperty%2520Crime%2520Residential%252CQuality%2520of%2520Life%252CRobbery%252CSexual%2520Assault%252CSexual%2520Offense%252CTheft%252CTheft%2520from%2520Vehicle%252CTheft%2520of%2520Vehicle&days=sunday%252Cmonday%252Ctuesday%252Cwednesday%252Cthursday%252Cfriday%252Csaturday&start_time=0&end_time=23&include_sex_offenders=false&current_tab=map&start_date=".concat(start_date, "&end_date=", end_date, "&lat=", val.latitude, "&lng=", val.longitude);
-    openInNewTab(url);
+    // openInNewTab(url);
 
-    function openInNewTab(url) {
-      window.open(url, '_blank');
-    }
+    // function openInNewTab(url) {
+    //   window.open(url, '_blank');
+    // }
+    var node = dom.byId("crimeData");
+    node.innerHTML = "".concat("<iframe src='", url, "' height='400' width='100%'></iframe>");
+
 
   }
 
+  
 
 });
