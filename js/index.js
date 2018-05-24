511c4aa0499d6a6f2a1548815946f2c12b1b7bb6
HCHEN <hchen@garlandtx.gov>
2018-05-24 12:33:56 -0500

    Update the color of noscript error. Use css for fontAwesome instead of js. Update button Planning &amp; Development / Zoning, make it wrap when the screen is too small.

--- js/index.js
+++ js/index.js
@@ -626,8 +626,7 @@ require([
     console.log("crime map:", url);
     var node = dom.byId("crimeData");
     node.innerHTML = "".concat("<iframe id='crimeDataIFrame' src='", url, "' height='400' width='100%'></iframe>");
-
-    dom.byId("crime-map-title").innerHTML = "".concat(" <i class='fas fa-plus-square'></i> Crime ( ", start_date, " to ", end_date, " )");
+    dom.byId("crime-map-title").innerHTML = "".concat(" <i class='fas fa-plus-square fa-lg'>&nbsp&nbspCrime ( ", start_date.slice(5), " to ", end_date.slice(5), " )</i>");
 
     dom.byId("open-crime-map").onclick = function () {
       window.open(url, '_blank');
