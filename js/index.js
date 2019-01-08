6394dee3cef757f44a878ef236496bb9018b36b4
huajunchen <hj.ch.chen@gmail.com>
2019-01-08 14:14:20 -0600

    Remove multi-search widget. Use multi-search object. Clean code.

--- js/index.js
+++ js/index.js
@@ -14,8 +14,13 @@ require([
   "esri/tasks/GeometryService",
   "esri/tasks/support/ProjectParameters",
 
+  "esri/geometry/SpatialReference",
+  "esri/geometry/geometryEngineAsync",
+
   "esri/Graphic",
 
+  "dojo/_base/lang",
+  "dojo/promise/all",
   "dojo/topic",
   "dojo/query",
   "dojo/dom-construct",
@@ -23,17 +28,18 @@ require([
   "dojo/text!/app-config/mygarland/config.json",
   "dojo/text!/app-config/mygarland/multilayers.json",
 
-  'js/multi-search.js',
   'dojo/domReady!'
 ], function (
-   dom, domClass, 
+  dom, domClass,
   Map, MapView, MapImageLayer,
 
   Search, Locator, Query, QueryTask,
-  GeometryService,  ProjectParameters,
+  GeometryService, ProjectParameters,
+  SpatialReference, geometryEngineAsync,
   Graphic,
 
-  topic, domQuery,  domConstruct,
+  lang, all,
+  topic, domQuery, domConstruct,
 
   config_json, multilayers_json
 
@@ -60,23 +66,201 @@ require([
     });
 
     //search widget
+    var searchSources = appSetting.locator.sourceSetting;
+    searchSources.locator = new Locator({
+      url: appSetting.locator.locatorUrl
+    });
     search = new Search({
       view: view,
       container: "search",
       allPlaceholder: ".",
       locationEnabled: false,
-      sources: [
-        dojo.mixin({
-          locator: new Locator({
-            url: appSetting.locator.locatorUrl
-          })
-        }, appSetting.locator.sourceSetting)
-      ]
+      sources: [searchSources]
     });
 
     //create multisearch widget.
-    multiSearch = new GetMultiSearch(layerSetting.layers);
-    multiSearch.startup();
+    multiSearch = {
+      mapService: layerSetting.layers.mapService, //need address map service and parcel map service
+      individualCityFacility: layerSetting.layers.individualCityFacility, //Police Station,Court ...
+      cityFacilitySourceList: layerSetting.layers.cityFacilitySourceList,
+      cityFacilityList: layerSetting.layers.cityFacilityList,
+      serviceZoneSourceList: layerSetting.layers.serviceZoneSourceList,
+      spatialReference: new SpatialReference({
+        wkid: 2276
+      }),
+      startNewSearch: function () {
+        this.searchResult = {
+          address: null,
+          addressID: null,
+          nearestCityFacilityList: [],
+          serviceZoneList: [],
+          parcelInfo: null
+        };
+      },
+      prepareCityFacilityList: function () {
+        var that = this;
+        this.cityFacilityList = [];
+        this.cityFacilitySourceList.forEach(function (item) {
+          runQuery(item);
+        });
+
+        function runQuery(queryParameter) {
+          var query = new Query({
+            where: queryParameter.where,
+            returnGeometry: true,
+            outFields: ["*"]
+          });
+          var queryTask = new QueryTask({
+            url: queryParameter.url
+          });
+          queryTask.execute(query).then(function (results) {
+            // Results.graphics contains the graphics returned from query
+            queryParameter.features = results.features;
+            that.cityFacilityList.push(queryParameter);
+          });
+        }
+      },
+      getInforByAddressID: function () {
+        console.log("getInforByAddressID function");
+        var that = this;
+        var query = new Query();
+        var queryTask = new QueryTask({
+          url: this.mapService.address
+        });
+        query.where = "ADDRESSID =" + this.searchResult.addressID;
+        //query.outSpatialReference = spatialReference2276;
+        query.returnGeometry = false;
+        query.outFields = ["PARCELID"];
+        queryTask.execute(query).then(function (results) {
+          // Results.graphics contains the graphics returned from query
+          if (results.features[0].attributes.PARCELID) {
+            var parcelID = results.features[0].attributes.PARCELID;
+            var query = new Query();
+            var queryTask = new QueryTask({
+              url: that.mapService.parcel
+            });
+            query.where = "PARCELID  =" + parcelID;
+            //query.outSpatialReference = spatialReference2276;
+            query.returnGeometry = false;
+            query.outFields = ["*"];
+            queryTask.execute(query).then(function (results) {
+              that.searchResult.parcelInfo = results.features[0].attributes;
+              topic.publish("multiSearch/parcelInfoUpdated", {
+                parcelID: parcelID
+              });
+            });
+          } else {
+            console.log("error getInforByAddressID: ", that.searchResult.addressID);
+          }
+        });
+      },
+      getNearestCityFacilityList: function () {
+        console.log("getNearestCityFacilityList Function");
+        var that = this;
+
+        //individualCityFacility distance
+        var arr = distanceToIndividualCityFacility(this.geometry);
+        this.searchResult.nearestCityFacilityList = this.searchResult.nearestCityFacilityList.concat(arr);
+        this.cityFacilityList.forEach(function (item) {
+          findNearest(that.geometry, item);
+        });
+
+        function findNearest(geometry, featureSet) {
+          var distanceRequestList = featureSet.features.map(function (item) {
+            return geometryEngineAsync.distance(geometry, item.geometry, "miles")
+          });
+          var promises = new all(distanceRequestList);
+          promises.then(lang.hitch(this, function (response) {
+            var minDistance = Math.min.apply(null, response);
+            var minIndex = response.indexOf(minDistance);
+            var minFeature = featureSet.features[minIndex];
+            featureSet.nearestFeature = minFeature.attributes;
+            featureSet.distance = minDistance.toFixed(2);
+            that.searchResult.nearestCityFacilityList.push(featureSet);
+            if (that.searchResult.nearestCityFacilityList.length == that.cityFacilityList.length + that.individualCityFacility.length) {
+              //time to display data
+              topic.publish("multiSearch/nearestCityFacilityUpdated", {
+                count: that.searchResult.nearestCityFacilityList.length
+              });
+            }
+          })).catch(function (e) {
+            console.log("Error - findNearest: ", e);
+          });
+        }
+
+        function distanceToIndividualCityFacility(geometry) {
+          var userPnt = {
+            x: geometry.x,
+            y: geometry.y
+          };
+          var arr = that.individualCityFacility.map(function (val) {
+            var facilityPnt = {
+              x: val.nearestFeature.x,
+              y: val.nearestFeature.y
+            };
+            return {
+              "title": val.title,
+              "displayID": val.displayID,
+              "nearestFeature": val.nearestFeature,
+              "distance": distanceBetweenTwoPointInStatePlan(userPnt, facilityPnt)
+            };
+          });
+          return arr;
+        }
+
+        function distanceBetweenTwoPointInStatePlan(pnt1, pnt2) {
+          return (Math.sqrt(Math.pow((pnt1.x - pnt2.x), 2) + Math.pow((pnt1.y - pnt2.y), 2)) / 5280).toFixed(2);
+        }
+      },
+      getServiceZoneList: function () {
+        console.log("getServiceZoneList Function");
+        var that = this;
+
+        var query = new Query({
+          returnGeometry: true,
+          outFields: ["*"],
+          spatialRelationship: "intersects"
+        });
+        query.geometry = this.geometry;
+
+        var queryRequest = [];
+        this.serviceZoneSourceList.forEach(function (item) {
+          var queryTask = new QueryTask({
+            url: item.url
+          });
+          //Assign the esriRequest execute function to a variable
+          var request = queryTask.execute(query);
+          //add the function variable to a array
+          queryRequest.push(request);
+        });
+
+        var promises = new all(queryRequest);
+        promises.then(function (response) {
+          for (var i in response) {
+            var featureSet = that.serviceZoneSourceList[i];
+            var result = {
+              id: featureSet.id,
+              title: featureSet.name,
+              containerID: featureSet.containerID,
+              displayID: featureSet.displayID,
+              queryPolygonCount: response[i].features.length
+            };
+            if (response[i].features.length > 0) {
+              result.serviceZone = response[i].features[0].attributes;
+              result.displayFieldName = response[i].displayFieldName;
+            }
+            that.searchResult.serviceZoneList.push(result);
+          }
+
+          //time to display data
+          topic.publish("multiSearch/serviceZoneListUpdated", {
+            count: response.length
+          });
+        }).catch(function (e) {
+          console.log("Error - getServiceZoneList:", e);
+        });
+      }
+    };
     multiSearch.prepareCityFacilityList();
 
     //set geometry service to match spatial reference on different service.
@@ -120,11 +304,10 @@ require([
     console.log("multiSearch/parcelInfoUpdated");
     var item = multiSearch.searchResult.parcelInfo;
     var obj = layerSetting.parcelData.map(function (val) {
-      return dojo.mixin({
-        serviceZone: {
-          value: item[val.fieldName]
-        }
-      }, val);
+      val.serviceZone = {
+        value: item[val.fieldName]
+      };
+      return val;
     });
     displayReferenceData(obj, "first");
   });
@@ -281,11 +464,9 @@ require([
             url = url.replace(new RegExp(val.replaceWith, 'g'), newValue);
           });
 
-          valueNodeProperty = dojo.mixin({
-            "href": url,
-            "target": "_blank",
-            "title": "Open to see details"
-          }, valueNodeProperty);
+          valueNodeProperty.href = url;
+          valueNodeProperty.target = "_blank";
+          valueNodeProperty.title = "Open to see details";
           domConstruct.create("a", valueNodeProperty, li);
         } else {
           domConstruct.create("span", valueNodeProperty, li);
@@ -792,7 +973,7 @@ require([
       divLegend.innerHTML = "";
       displayLegend(appSetting.subMap.streetCondition.legend, divLegend);
     } else {
-      
+
       domClass.remove(divLegend, "black-border");
 
       showSubMap(multiSearch.searchResult.addressGeometry, [new MapImageLayer(appSetting.subMap.baseMap.map)]);
