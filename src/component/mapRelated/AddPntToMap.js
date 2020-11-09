8d9b41352af6ad85b58035bbe9c16fa67eaec759
huajunchen <hj.ch.chen@gmail.com>
2020-11-09 14:48:37 -0600

    fixed all map problems!!!

--- src/component/mapRelated/AddPntToMap.js
+++ src/component/mapRelated/AddPntToMap.js
@@ -1,31 +1,34 @@
-import React, { useEffect, useRef, PureComponent } from 'react';
+import React, { useEffect, useRef, PureComponent, Component } from 'react';
 import { loadModules } from 'esri-loader';
 
 class AddPoint extends PureComponent {
 
     addPoint(mapviews, geometry) {
-        loadModules(["esri/tasks/support/ProjectParameters", "esri/tasks/GeometryService", "esri/Graphic"])
-            .then(([ProjectParameters, GeometryService, Graphic]) => {
+        console.log(mapviews)
+        loadModules(["esri/tasks/support/ProjectParameters", "esri/tasks/GeometryService", "esri/geometry/SpatialReference", "esri/Graphic"])
+            .then(([ProjectParameters, GeometryService, SpatialReference, Graphic]) => {
                 var geomSer = new GeometryService("https://maps.garlandtx.gov/arcgis/rest/services/Utilities/Geometry/GeometryServer");
-                mapviews.forEach(mapview => {
-                    var params = new ProjectParameters({
-                        geometries: [geometry],
-                        outSpatialReference: mapview.view.spatialReference,
+                const mapviewSpatialReference = new SpatialReference({ wkid: 3857 });
+                var params = new ProjectParameters({
+                    geometries: [geometry],
+                    outSpatialReference: mapviewSpatialReference,
+                });
+                geomSer.project(params).then((result) => {
+                    var pnt = new Graphic({
+                        geometry: result[0],
+                        symbol: {
+                            type: "simple-marker",
+                            color: "#dc2533"
+                        }
                     });
 
-                    geomSer.project(params).then((result) => {
-                        var pnt = new Graphic({
-                            geometry: result[0],
-                            symbol: {
-                                type: "simple-marker",
-                                color: "#dc2533"
-                            }
-                        });
+                    mapviews.forEach(mapview => {
                         mapview.view.graphics.removeAll();
                         mapview.view.graphics.add(pnt);
                         mapview.view.center = [result[0].longitude, result[0].latitude];
                     });
-                })
+                });
+
 
             }
             )
@@ -37,13 +40,14 @@ class AddPoint extends PureComponent {
         })
     }
     componentDidMount() {
+        console.log('mount', this.props.geometry)
         if (this.props.geometry) {
             this.addPoint(this.props.mapviews, this.props.geometry);
         }
     }
 
     componentDidUpdate() {
-        debugger;
+        console.log('update', this.props.geometry)
         if (this.props.geometry) {
             this.addPoint(this.props.mapviews, this.props.geometry);
         } else {
