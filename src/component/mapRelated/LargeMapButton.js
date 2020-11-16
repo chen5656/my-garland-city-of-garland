83b540edd18844ce62b5f145ecd796d12aba5d47
huajunchen <hj.ch.chen@gmail.com>
2020-11-16 12:40:32 -0600

    temp

--- src/component/mapRelated/LargeMapButton.js
+++ src/component/mapRelated/LargeMapButton.js
@@ -6,7 +6,18 @@ import CloseIcon from '@material-ui/icons/Close';
 import ZoomOutMapIcon from '@material-ui/icons/ZoomOutMap';
 
 
-function getModalStyle() {
+
+
+function getModalStyle(smScreen) {
+  if(smScreen){
+    return {
+      top: '0',
+      left: '0',
+      // transform: `translate(-${top}%, -${left}%)`,
+      width: '100%',
+      height: '100%',
+    };
+  }
   return {
     top: '5%',
     left: '5%',
@@ -34,9 +45,10 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SimpleModal(props) {
+  const smScreen = (window.innerWidth<576);
   const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
+  const [modalStyle] = React.useState(getModalStyle(smScreen));
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => {
@@ -47,13 +59,18 @@ export default function SimpleModal(props) {
     setOpen(false);
   };
 
-
   return (
     //className=' d-none d-sm-block '
     <div >
-      <Button size='small' variant='contained' color="primary" onClick={handleOpen} title= 'Show Large Map'>
-        Show Large
-      </Button>
+      {smScreen ?
+        <Button  size='small' variant='contained' color="primary" onClick={handleOpen} >
+          <ZoomOutMapIcon />
+        </Button>
+        :
+        <Button size='small' variant='contained' color="primary" onClick={handleOpen} title='Show Large Map'>
+          Show Large
+        </Button>
+      }
       <Modal
         open={open}
         onClose={handleClose}
