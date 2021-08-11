d31679145db5788ba8b321d022bfda84a2c7d7c8
huajunchen <35971289+chen5656@users.noreply.github.com>
2021-08-11 13:17:30 -0500

    pavement map - ready
    pavement map - ready

---
+++ my-garland-react/src/component/MapRelated/LargeMapButton1.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
+import CloseIcon from '@material-ui/icons/Close';
+import ZoomOutMapIcon from '@material-ui/icons/ZoomOutMap';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  closeBtn: {
+    position: 'absolute',
+    zIndex: 102,
+    right: '-15px',
+    top: 0,
+  }
+}));
+
+export default function SimpleModal(props) {
+  const smScreen = (window.innerWidth < 576);
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  
+  return (
+    <div >
+      {smScreen ?
+        <Button size='small' variant='contained' color="primary" onClick={handleOpen} >
+          <ZoomOutMapIcon />
+        </Button>
+        :
+        <Button size='small' variant='contained' color="primary" onClick={handleOpen} title='Show Large Map'>
+          Show Large
+        </Button>
+      }
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+            <div style={smScreen ?
+      { top: '0', left: '0', width: '100%', height: '100%', } :
+      { top: '5%', left: '5%', width: '85%', height: '85%', }}
+      className={classes.paper}>
+      <Button onClick={handleClose} className={classes.closeBtn} >
+        <CloseIcon />
+      </Button>
+      {props.body}
+    </div>
+      </Modal>
+    </div>
+  );
+}
--- my-garland-react/src/component/SearchResult/MapResult/PavementMap.js
+++ my-garland-react/src/component/SearchResult/MapResult/PavementMap.js
@@ -3,16 +3,15 @@ import React, { useEffect, useRef ,useState} from 'react';
 import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
 import MapImageLayer from "@arcgis/core/layers/MapImageLayer";
-import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 import Legend from '@arcgis/core/widgets/Legend';
 import LayerList from '@arcgis/core/widgets/LayerList';
 import Graphic from '@arcgis/core/Graphic';
-import { PanoramaVertical } from '@material-ui/icons';
 import StreetConditionLegendToggle from './StreetConditionLegendToggle';
 
+import LargeMapButton from '../../MapRelated/LargeMapButton1';
 const PavementMap = (props) => {
     const mapDiv = useRef(null);
-    const [layerOn,setLayerOn]=useState(true);
+    const showLargeBtn=useRef(null);
     const [mapView, setView] = useState(null);
     const [thisMap, setMap] = useState(null);
   
@@ -35,45 +34,96 @@ const PavementMap = (props) => {
                 map: map,
                 center: props.mapPoint.geometry,
                 zoom: 15,
-            });
+            });            
             setMap(map);
             setView(view);
+            if(props.largerVersion){
+                showLargeBtn.current.style.display='none';
+                mapDiv.current.style.height='100%';
+                var legend = new Legend({
+                    view: view
+                  });            
+                  view.ui.add(legend, "bottom-left");
+                  var layerList = new LayerList({
+                    view: view
+                  });            
+                  view.ui.add(layerList, "top-right");
+
+            }else{
+                view.ui.add(showLargeBtn.current , 'bottom-right');
+            }
+            if(props.mapPoint){
+                addSearchPnt(props.mapPoint.geometry,props.mapPoint.fullAddress,view);
+            }
+            
         }
      },[]  );
     useEffect(() => {   
         if(mapView&&props.mapPoint){
+            addSearchPnt(props.mapPoint.geometry,props.mapPoint.fullAddress,mapView);    
+        }
+
+     },[props.mapPoint]  );
+
+     
+    useEffect(() => {   
+        if(mapView){
             let layer=
                  new MapImageLayer({
                     'url': 'https://maps.garlandtx.gov/arcgis/rest/services/WebApps/MyGarland/MapServer',
                     'sublayers':  [
                         { "id": 5, "visible": true, title: 'Parcels' },
                         { "id": 4, "visible": true,title: 'Address' },
-                        { "id": 37, "visible": layerOn, title: 'pavement-condition' }
+                        { "id": 37, "visible": props.layerOn, title: 'pavement-condition' }
                     ],
                   });
             thisMap.removeAll();
             thisMap.add(layer)
-            var pnt = new Graphic({
-                geometry: props.mapPoint.geometry,
-                symbol: {
-                    type: "simple-marker",
-                    color: "#dc2533"
-                }
-            });
-            pnt.popupTemplate = {
-                content: props.mapPoint.fullAddress,
-            };
-            mapView.graphics.removeAll();
-            mapView.graphics.add(pnt);
-            mapView.center = props.mapPoint.geometry;
-        }
 
-     },[props.mapPoint,layerOn]  );
+        }
+    },[props.mapPoint,props.layerOn]  );
+
+    const addSearchPnt=(geometry,popupContent,view)=>{
+        var pnt = new Graphic({
+            geometry: geometry,
+            symbol: {
+                type: "simple-marker",
+                color: "#dc2533"
+            }
+        });
+        pnt.popupTemplate = {
+            content:popupContent ,
+        };
+        view.graphics.removeAll();
+        view.graphics.add(pnt);
+        view.center = geometry;
+    }
     
-    return (<div className='px-2'>
-      <div className='webmap' ref={mapDiv} style={{ height:  '300px' }} />
-      <StreetConditionLegendToggle layerOn={layerOn} setLayerOn={setLayerOn}/>
-    </div>)
-  };
+    return <>
+    <div className='webmap' ref={mapDiv} style={{ height:  '300px' }} />
+    <div  ref={showLargeBtn} >
+        <LargeMapButton
+            name='crime-map'
+            body={<PavementMap 
+                mapPoint={props.mapPoint} 
+                largerVersion={true}
+
+                />}
+        /></div>
+    </>
+      
   
-  export default PavementMap;
+};
+
+const PavementDiv = (props) => {
+    const [layerOn,setLayerOn]=useState(true);
+    return (<div className='px-2'>
+        <PavementMap layerOn={layerOn} setLayerOn={setLayerOn} mapPoint={ props.mapPoint}/>
+        <StreetConditionLegendToggle layerOn={layerOn} setLayerOn={setLayerOn}/>
+       
+    </div>)
+
+}
+
+  
+  export default PavementDiv;
